a9eedcab5453d880c635889e97a262bb10b71f4b
ebubekeyz <ebubeofforjoe@gmail.com>
2023-09-21 19:34:24 +0100

    second

--- public/js/withdrawal.js
+++ public/js/withdrawal.js
@@ -14,15 +14,49 @@ const formatPrice = (investment) => {
   }
 
 const chevronDOM = document.querySelector('.chevron')
+const accountInput= document.querySelector('#realname')
+
 
 chevronDOM.addEventListener('click',() => {
     window.history.back()
 })
 
-const accountInput = document.querySelector('.realname')
 
-window.addEventListener('DOMContentLoaded',() => {
-    try
+
+const params2 = window.location.search;
+const urlID = new URLSearchParams(params2).get('id');
+
+
+window.addEventListener('DOMContentLoaded', async () => {
+
+  try{
+   
+    const response = await fetch(`/api/v1/bankInfo/${urlID}/bankInfo`,{
+      method: 'GET',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+     
+    })
+    const data = await response.json()
+    
+    const bankInfo = data.bankInfo
+    
+    const length = bankInfo.length - 1
+    
+    if(response.status === 200){
+     
+        accountInput.value = bankInfo[length].bankAccount
+     
+         
+    }
+    else {
+      console.log(data.msg)
+    }
+  }
+  catch(error){
+    console.log(error)
+  }
 })
 
 
